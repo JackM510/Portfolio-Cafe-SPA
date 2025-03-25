07169e10817020866c07c73205354cae5fbfd480
JackM <JackM@placeholder.com>
2025-03-25 17:55:32 +1100

    Added a fade on refresh to the landing span and landing img

---
+++ js/landing.js
@@ -0,0 +1,8 @@
+// Wait for the page to load
+window.addEventListener('load', function() {
+    const spanElement = document.getElementById('landing-span');
+    spanElement.classList.add('landingSpanfade');
+
+    const spanElement2 = document.getElementById('landing-img');
+    spanElement2.classList.add('landingImgfade');
+});
