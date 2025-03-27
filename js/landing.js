854801590e4d63084dddbc5981c6f09fe72a8cc5
JackM <JackM@placeholder.com>
2025-03-27 12:00:13 +1100

    Made changes to text font-size for multiple media conditions, made adjustments to the landing sections span on smaller screens, and fixed an issue with a shadow in the menu tab-panes

--- js/landing.js
+++ js/landing.js
@@ -1,8 +1,8 @@
 // Wait for the page to load
 window.addEventListener('load', function() {
     const spanElement = document.getElementById('landing-span');
-    spanElement.classList.add('landingSpanfade');
+    spanElement.classList.add('landingSpanFade');
 
     const spanElement2 = document.getElementById('landing-img');
-    spanElement2.classList.add('landingImgfade');
+    spanElement2.classList.add('landingImgFade');
 });
