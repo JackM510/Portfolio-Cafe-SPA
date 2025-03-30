d13848cc17dc5cb1132ee08bd44a26e6ec9ac655
JackM <JackM@placeholder.com>
2025-03-30 18:26:12 +1100

    Re-wrote nav.css with a mobile first approach which resizes for desktop displays

--- js/nav.js
+++ js/nav.js
@@ -1,22 +1,14 @@
 // Navbar Collapse
 document.addEventListener('DOMContentLoaded', function () {
-    let navLinks = document.querySelectorAll('.nav-link');
-    let navBtn = document.querySelector('#nav-btn');
+    let navElements = document.querySelectorAll('.nav-li, #nav-btn');
     let navCollapse = document.getElementById('collapsible-navbar');
 
-    navLinks.forEach(function (link) {
-        link.addEventListener('click', function () {
+    navElements.forEach(function (element) {
+        element.addEventListener('click', function () {
             let bsCollapse = new bootstrap.Collapse(navCollapse, {
                 toggle: false
             });
             bsCollapse.hide();
         });
-    });
-
-    navBtn.addEventListener('click', function () {
-        let bsCollapse = new bootstrap.Collapse(navCollapse, {
-            toggle: false
-        });
-        bsCollapse.hide();
     });
 });
