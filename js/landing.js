23af63007490469fc8f8915787360dd81bd7b5ac
JackM <JackM@placeholder.com>
2025-03-30 20:46:12 +1100

    Added aria label to landing section

--- js/landing.js
+++ js/landing.js
@@ -1,9 +1,9 @@
 // Wait for the page to load
 window.addEventListener('load', function() {
-    // Add fade to the landing-span
+    // Add fade to landing-span
     const landingSpan = document.getElementById('landing-span');
     landingSpan.classList.add('span-fade');
-    // Add fade to the landing-img
+    // Add fade to landing-img
     const landingImg = document.getElementById('landing-img');
     landingImg.classList.add('img-fade');
 });
