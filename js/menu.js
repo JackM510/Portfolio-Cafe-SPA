2964397bc581cdf6b30b1a5fb891ef977ee257f1
JackM <JackM@placeholder.com>
2025-04-04 11:54:14 +1100

    Moved BS5 classes into CSS for elements with repetitive HTML classes and unique IDs. Changed CSS to mobile-first design & simplified some of the JS logic for better readability and structure

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,9 @@
-let menu = {};
+let menu = {}; // Stores the menu data
+// Menu styles for 'new' & 'popular' badges
+const badgeStyles = {
+  NEW: "bg-success",
+  POPULAR: "bg-warning",
+};
 
 // Fetch the JSON file
 fetch('./data/menu.json')
@@ -9,109 +14,103 @@ fetch('./data/menu.json')
         return response.json(); // Parse the JSON file
     })
     .then(data => {
-        menu = data.Menu; // Access the 'Menu' object from your JSON
-        // Call the function for each menu category
-        populateCategory("Breakfast", "menu-breakfast");
-        populateCategory("Lunch", "menu-lunch");
-        populateCategory("Drinks", "menu-drinks");
+        menu = data.Menu; // Access the 'Menu' object from menu.json
+        populateCategory("Breakfast", "menu-container-breakfast");
+        populateCategory("Lunch", "menu-container-lunch");
+        populateCategory("Drinks", "menu-container-drinks");
 
-        // Manually trigger Bootstrap's tab logic for the default active tab
-        const defaultTab = document.querySelector('a[href="#tab1"]'); // Breakfast tab
+        // Manually set the active menu tab to the Breakfast tab
+        const defaultTab = document.getElementById('menu-link-breakfast');
         const bootstrapTab = new bootstrap.Tab(defaultTab);
-        bootstrapTab.show(); // Simulates a user clicking the Breakfast tab
+        bootstrapTab.show(); // Simulates clicking the Breakfast tab
 
     })
     .catch(error => console.error('Error:', error));
 
-
+// Populate each menu category with menu data
 const populateCategory = (category, containerId) => {
-    const container = document.getElementById(containerId);
-    const items = menu[category];
-    let count = 0;
-  
-    if (items && container) {
-      // Check if the category has subcategories (like Drinks)
-      if (typeof items === "object" && !Array.isArray(items)) {
-        // Handle subcategories
-        Object.keys(items).forEach(subcategory => {
-          // Create a single column for the subcategory
-          const headingColumn = document.createElement("div");
-          headingColumn.classList.add("col-12", "text-center", "mb-4");
-          
-          // Add a heading for the subcategory
-          const heading = document.createElement("h4");
-          if (count == 0) {
-            heading.classList.add("heading-underline", "mt-4", "p-3", "subcategory-heading");
-          } else {
-            heading.classList.add("heading-underline", "mt-3", "p-3", "subcategory-heading");
-          }
-          heading.textContent = subcategory;
-          headingColumn.appendChild(heading);
-          count += 1;
+  const container = document.getElementById(containerId);
+  const items = menu[category];
+  let subheadingCount = 0; // Determine the first subcategory and apply extra margin-top for spacing
+
+  if (items && container) {
+    // Check if the menu category has any subcategories
+    if (typeof items === "object" && !Array.isArray(items)) {
+      Object.keys(items).forEach(subcategory => {
+        // Create a BS5 column for each subcategory
+        const subcategoryColumn = document.createElement("div");
+        subcategoryColumn.classList.add("col-12", "text-center", "mb-4");
+        
+        // Add the subheading to the subcategory div
+        const heading = document.createElement("h4");
+        heading.classList.add("subcategory-heading", "heading-underline", subheadingCount === 0 ? "mt-4" : "mt-3");
+        heading.textContent = subcategory;
+        subcategoryColumn.appendChild(heading);
+        subheadingCount++;
 
         // Append the subcategory column to the main container
-        container.appendChild(headingColumn);
-  
-          items[subcategory].forEach(item => {
-            createMenuCard(item, container); // Reusable card creation
-          });  
-        });
-      } else {
-        // Handle categories without subcategories (like Breakfast or Lunch)
-        items.forEach(item => {
-          createMenuCard(item, container); // Reusable card creation
-        });
-      }
+        container.appendChild(subcategoryColumn);
+        // Append each subcategory item to the container
+        items[subcategory].forEach(item => {
+          createMenuCard(item, container);
+        });  
+      });
+    } 
+    else { // Default else statement if there is no subcategory
+      items.forEach(item => {
+        createMenuCard(item, container);
+      });
     }
-  };
+  }
+};
   
-  
-  const createMenuCard = (item, container) => {
-    // Create the card container
-    const card = document.createElement("div");
-    card.classList.add("col-12", "card", "rounded-0", "border-0", "px-5", "menu-card");
-  
-    const cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
-  
-    // Card title (item name and price)
-    const cardTitle = document.createElement("h5");
-    cardTitle.classList.add("card-title", "align-items-center");
-    cardTitle.innerHTML = `
-      ${item.name} <span class="menu-price float-end">${item.price}</span>
-    `;
+// Create each menu item as a BS5 card
+const createMenuCard = (item, container) => {
+  // Create the card container
+  const card = document.createElement("div");
+  card.classList.add("col-12", "card", "menu-card");
 
-    // Dietary Span
-    if (item.dietary) {
-      const dietSpan = document.createElement('span');
-      dietSpan.textContent = item.dietary;
-      dietSpan.classList.add("badge", "diet-span");
-      cardTitle.appendChild(dietSpan);
-    }
-  
-    // Card description
-    const cardDescription = document.createElement("p");
-    cardDescription.classList.add("card-text");
-    cardDescription.textContent = item.description;
-  
-    // Add badge (if exists)
-    if (item.badge) {
-      const badge = document.createElement("span");
-      badge.textContent = item.badge;
+  // Create the card body
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
 
-      if (badge.textContent == 'NEW') {
-        badge.classList.add("badge", "bg-success", "menu-badge");
-      } else if (badge.textContent == 'POPULAR') {
-        badge.classList.add("badge", "bg-warning", "menu-badge");
-      }
-      cardTitle.appendChild(badge);
-    }
-  
-    // Assemble the card
-    cardBody.appendChild(cardTitle);
-    cardBody.appendChild(cardDescription);
-    card.appendChild(cardBody);
-  
-    // Append the card to the provided container
-    container.appendChild(card);
-  }; 
+  // Create the card title (holds the item name, badges and price)
+  const cardTitle = document.createElement("h5");
+  cardTitle.classList.add("card-title", "align-items-center");
+  cardTitle.innerHTML = `${item.name}`;
+
+  // Create the dietary badge
+  if (item.dietary) {
+    const dietSpan = document.createElement('span');
+    dietSpan.textContent = item.dietary;
+    dietSpan.classList.add("badge", "diet-badge");
+    cardTitle.appendChild(dietSpan);
+  }
+
+  // Create the 'new' & 'popular' badges
+  if (item.badge && badgeStyles[item.badge]) {
+    const badge = document.createElement("span");
+    badge.textContent = item.badge;
+    badge.classList.add("badge", badgeStyles[item.badge], "item-badge");
+    cardTitle.appendChild(badge);
+  }
+
+  // Create the price span
+  const priceSpan = document.createElement("span");
+  priceSpan.classList.add("menu-price", "float-end");
+  priceSpan.textContent = item.price;
+  cardTitle.appendChild(priceSpan);
+
+  // Card the description for each menu item
+  const cardDescription = document.createElement("p");
+  cardDescription.classList.add("card-text");
+  cardDescription.textContent = item.description;
+
+  // Append each card element to the card
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(cardDescription);
+  card.appendChild(cardBody);
+
+  // Append the card to the provided menu container
+  container.appendChild(card);
+}; 
